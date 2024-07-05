3b37140ffb145a4674deeb431d57a591a78e24c8
samndambuki <sam.ndambuki08@gmail.com>
2024-07-05 21:51:16 +0300

    worked on login code logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ApiService } from '../api.service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,9 +14,16 @@ export class LoginComponent {
     email: new FormControl(),
     password: new FormControl()
   })
-  constructor(private api: ApiService, private http: HttpClient) { }
+  constructor(private api: ApiService, private http: HttpClient, private router: Router) { }
   login() {
-    // const url = `${}`
-    // this.http.post()
+    const url = `${this.api.base_uri_api}login`
+    const formData = this.loginForm.value
+    this.http.post(url, { formData }, { withCredentials: true, observe: 'response' }).subscribe({
+      next: (response: HttpResponse<any>) => {
+        if (response.ok) {
+          this.router.navigate(['dashboard'])
+        }
+      }
+    })
   }
 }
