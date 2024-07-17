7bbd0fe679d97684cc874a25bfa02ec79c5eaa69
samndambuki <sam.ndambuki08@gmail.com>
2024-07-17 10:04:50 +0300

    working on edit student dialog

--- src/app/edit-student-dialog/edit-student-dialog.component.ts
+++ src/app/edit-student-dialog/edit-student-dialog.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { Student } from '../shared/interfaces/student.interface';
+import { ApiService } from '../api.service';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-student-dialog',
   templateUrl: './edit-student-dialog.component.html',
-  styleUrls: ['./edit-student-dialog.component.scss']
+  styleUrls: ['./edit-student-dialog.component.scss'],
 })
 export class EditStudentDialogComponent {
-  editStudentForm = new FormGroup({
-    first_name:new FormControl(),
-    last_name:new FormControl(),
-    date_of_birth:new FormControl(),
-    gender:new FormControl(),
-    address:new FormControl(),
-    phone_number:new FormControl()
-  })
+  editStudentForm: FormGroup;
+  constructor(
+    private dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public student: Student,
+    private fb: FormBuilder,
+    private api: ApiService,
+    private http: HttpClient,
+    private dialogRef: MatDialogRef<EditStudentDialogComponent>
+  ) {
+    this.editStudentForm = this.fb.group({
+      first_name: new FormControl(student.first_name),
+      last_name: new FormControl(student.last_name),
+      date_of_birth: new FormControl(student.date_of_birth),
+      gender: new FormControl(student.gender),
+      address: new FormControl(student.address),
+      phone_number: new FormControl(student.phone_number),
+    });
+  }
 
-  constructor(private dialog:MatDialog){}
-
+  edit_student() {
+    const url = `${this.api.base_uri_api}students/${this.student.id}`;
+    const formData = this.editStudentForm.value;
+    this.http
+      .put(url, formData, { withCredentials: true, observe: 'response' })
+      .subscribe({
+        next: (response: HttpResponse<any>) => {
+          if (response.ok) {
+            this.dialogRef.close();
+          }
+        },
+      });
+  }
 }
